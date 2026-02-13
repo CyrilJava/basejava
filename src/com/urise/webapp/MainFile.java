a48e00117781c930d912670a6d7dc6a732b7febb
Cyril <efimov-ka@narod.ru>
2026-02-13 17:38:11 +0300

    HW09 Main fixes (MainFile, ResumeTestData)

--- src/com/urise/webapp/MainFile.java
+++ src/com/urise/webapp/MainFile.java
@@ -8,10 +8,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
-/**
- * gkislin
- * 21.07.2016
- */
 public class MainFile {
     public static void main(String[] args) throws IOException {
         String filePath = ".\\.gitignore";
@@ -42,23 +38,22 @@ public class MainFile {
         System.out.println("--- 5. Walk - вывод всех файлов проекта ---");
         try (Stream<Path> stream = Files.walk(Paths.get(path))) {
             stream.filter(Files::isRegularFile)
-                    .forEach(System.out::println);// полное имя
-            //.forEach(x -> System.out.println(x.getFileName()));// только имя файла
+                    .forEach(System.out::println); // полное имя
+            // .forEach(x -> System.out.println(x.getFileName())); // только имя файла
         }
         System.out.println();
         System.out.println("--- 6. Tree - рекурсивный обход и вывод имени файлов в каталогах и подкаталогах---");
-        //String treePath = "../basejava/src";
-        //String treePath = "../basejava/src/com/urise/webapp";
+        // String treePath = "../basejava/src";
+        // String treePath = "../basejava/src/com/urise/webapp";
         String treePath = "D:\\Test";
         listDirectory(treePath, 0);
-        //listDirOnly(treePath, 0);
-
+        // listDirOnly(treePath, 0);
     }
 
-    //выводим дерево
+    // выводим дерево
     public static void listDirectory(String path, int level) {
         File[] rootLevel = new File(path).listFiles();
-        if ((rootLevel != null) && (rootLevel.length > 0)) {
+        if (rootLevel != null) {
             for (File file : rootLevel) {
                 for (int i = 0; i < level; i++) {
                     System.out.print("\t");
@@ -72,19 +67,4 @@ public class MainFile {
             }
         }
     }
-
-    /*public static void listDirOnly(String path, int level) {
-        File[] rootLevel = new File(path).listFiles();
-        if ((rootLevel != null) && (rootLevel.length > 0)) {
-            for (File file : rootLevel) {
-                if (file.isDirectory()) {
-                    for (int i = 0; i < level; i++) {
-                        System.out.print("\t");
-                    }
-                    System.out.println("╚ " + file.getName());
-                    listDirOnly(file.getAbsolutePath(), level + 1);
-                }
-            }
-        }
-    }*/
 }
--- src/com/urise/webapp/ResumeTestData.java
+++ src/com/urise/webapp/ResumeTestData.java
@@ -3,17 +3,14 @@ package com.urise.webapp;
 import com.urise.webapp.model.*;
 import com.urise.webapp.util.DateUtil;
 import com.urise.webapp.util.TransliterateUtil;
-
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class ResumeTestData {
-
     public static void main(String[] args) {
         Resume r = new Resume("1", "Кислин Григорий");
-        //Resume r = createTestResume("A","Ефимов Кирилл");
 
         r.addContact(ContactType.PHONE, "+7(921) 855-0482");
         r.addContact(ContactType.EMAIL, "gkislin@yandex.ru");
@@ -24,53 +21,53 @@ public class ResumeTestData {
         r.addSection(SectionType.OBJECTIVE, new TextSection("Ведущий стажировок и корпоративного обучения по Java Web и Enterprise технологиям"));
         r.addSection(SectionType.PERSONAL, new TextSection("Аналитический склад ума, сильная логика, креативность, инициативность. Пурист кода и архитектуры."));
 
-        List<String> achievList = new ArrayList<>();
-        achievList.add("Организация команды и успешная реализация Java проектов для сторонних заказчиков: приложения автопарк на стеке Spring Cloud/микросервисы, система мониторинга показателей спортсменов на Spring Boot, участие в проекте МЭШ на Play-2, многомодульный Spring Boot + Vaadin проект для комплексных DIY смет");
-        achievList.add("С 2013 года: разработка проектов \"Разработка Web приложения\",\"Java Enterprise\", \"Многомодульный maven. Многопоточность. XML (JAXB/StAX). Веб сервисы (JAX-RS/SOAP). Удаленное взаимодействие (JMS/AKKA)\". Организация онлайн стажировок и ведение проектов. Более 3500 выпускников.");
-        achievList.add("Реализация двухфакторной аутентификации для онлайн платформы управления проектами Wrike. Интеграция с Twilio, DuoSecurity, Google Authenticator, Jira, Zendesk.");
-        achievList.add("Налаживание процесса разработки и непрерывной интеграции ERP системы River BPM. Интеграция с 1С, Bonita BPM, CMIS, LDAP. Разработка приложения управления окружением на стеке: Scala/Play/Anorm/JQuery. Разработка SSO аутентификации и авторизации различных ERP модулей, интеграция CIFS/SMB java сервера.");
-        achievList.add("Реализация c нуля Rich Internet Application приложения на стеке технологий JPA, Spring, Spring-MVC, GWT, ExtGWT (GXT), Commet, HTML5, Highstock для алгоритмического трейдинга.");
-        achievList.add("Создание JavaEE фреймворка для отказоустойчивого взаимодействия слабо-связанных сервисов (SOA-base архитектура, JAX-WS, JMS, AS Glassfish). Сбор статистики сервисов и информации о состоянии через систему мониторинга Nagios. Реализация онлайн клиента для администрирования и мониторинга системы по JMX (Jython/ Django).");
-        achievList.add("Реализация протоколов по приему платежей всех основных платежных системы России (Cyberplat, Eport, Chronopay, Сбербанк), Белоруcсии(Erip, Osmp) и Никарагуа.");
-        ListSection achievement = new ListSection(achievList);
+        List<String> achievements = new ArrayList<>();
+        achievements.add("Организация команды и успешная реализация Java проектов для сторонних заказчиков: приложения автопарк на стеке Spring Cloud/микросервисы, система мониторинга показателей спортсменов на Spring Boot, участие в проекте МЭШ на Play-2, многомодульный Spring Boot + Vaadin проект для комплексных DIY смет");
+        achievements.add("С 2013 года: разработка проектов \"Разработка Web приложения\",\"Java Enterprise\", \"Многомодульный maven. Многопоточность. XML (JAXB/StAX). Веб сервисы (JAX-RS/SOAP). Удаленное взаимодействие (JMS/AKKA)\". Организация онлайн стажировок и ведение проектов. Более 3500 выпускников.");
+        achievements.add("Реализация двухфакторной аутентификации для онлайн платформы управления проектами Wrike. Интеграция с Twilio, DuoSecurity, Google Authenticator, Jira, Zendesk.");
+        achievements.add("Налаживание процесса разработки и непрерывной интеграции ERP системы River BPM. Интеграция с 1С, Bonita BPM, CMIS, LDAP. Разработка приложения управления окружением на стеке: Scala/Play/Anorm/JQuery. Разработка SSO аутентификации и авторизации различных ERP модулей, интеграция CIFS/SMB java сервера.");
+        achievements.add("Реализация c нуля Rich Internet Application приложения на стеке технологий JPA, Spring, Spring-MVC, GWT, ExtGWT (GXT), Commet, HTML5, Highstock для алгоритмического трейдинга.");
+        achievements.add("Создание JavaEE фреймворка для отказоустойчивого взаимодействия слабо-связанных сервисов (SOA-base архитектура, JAX-WS, JMS, AS Glassfish). Сбор статистики сервисов и информации о состоянии через систему мониторинга Nagios. Реализация онлайн клиента для администрирования и мониторинга системы по JMX (Jython/ Django).");
+        achievements.add("Реализация протоколов по приему платежей всех основных платежных системы России (Cyberplat, Eport, Chronopay, Сбербанк), Белоруcсии(Erip, Osmp) и Никарагуа.");
+        ListSection achievement = new ListSection(achievements);
         r.addSection(SectionType.ACHIEVEMENT, achievement);
 
-        List<String> qualList = new ArrayList<>();
-        qualList.add("JEE AS: GlassFish (v2.1, v3), OC4J, JBoss, Tomcat, Jetty, WebLogic, WSO2");
-        qualList.add("Version control: Subversion, Git, Mercury, ClearCase, Perforce");
-        qualList.add("DB: PostgreSQL(наследование, pgplsql, PL/Python), Redis (Jedis), H2, Oracle, MySQL, SQLite, MS SQL, HSQLDB");
-        qualList.add("Languages: Java, Scala, Python/Jython/PL-Python, JavaScript, Groovy");
-        qualList.add("XML/XSD/XSLT, SQL, C/C++, Unix shell scripts");
-        qualList.add("Java Frameworks: Java 8 (Time API, Streams), Guava, Java Executor, MyBatis, Spring (MVC, Security, Data, Clouds, Boot), JPA (Hibernate, EclipseLink), Guice, GWT(SmartGWT, ExtGWT/GXT), Vaadin, Jasperreports, Apache Commons, Eclipse SWT, JUnit, Selenium (htmlelements).");
-        qualList.add("Python: Django.");
-        qualList.add("JavaScript: jQuery, ExtJS, Bootstrap.js, underscore.js");
-        qualList.add("Scala: SBT, Play2, Specs2, Anorm, Spray, Akka");
-        qualList.add("Технологии: Servlet, JSP/JSTL, JAX-WS, REST, EJB, RMI, JMS, JavaMail, JAXB, StAX, SAX, DOM, XSLT, MDB, JMX, JDBC, JPA, JNDI, JAAS, SOAP, AJAX, Commet, HTML5, ESB, CMIS, BPMN2, LDAP, OAuth1, OAuth2, JWT.");
-        qualList.add("Инструменты: Maven + plugin development, Gradle, настройка Ngnix");
-        qualList.add("администрирование Hudson/Jenkins, Ant + custom task, SoapUI, JPublisher, Flyway, Nagios, iReport, OpenCmis, Bonita, pgBouncer");
-        qualList.add("Отличное знание и опыт применения концепций ООП, SOA, шаблонов проектирования, архитектурных шаблонов, UML, функционального программирования");
-        qualList.add("Родной русский, английский \"upper intermediate\"");
-        ListSection qualifications = new ListSection(qualList);
-        r.addSection(SectionType.QUALIFICATIONS, qualifications);
+        List<String> qualifications = new ArrayList<>();
+        qualifications.add("JEE AS: GlassFish (v2.1, v3), OC4J, JBoss, Tomcat, Jetty, WebLogic, WSO2");
+        qualifications.add("Version control: Subversion, Git, Mercury, ClearCase, Perforce");
+        qualifications.add("DB: PostgreSQL(наследование, pgplsql, PL/Python), Redis (Jedis), H2, Oracle, MySQL, SQLite, MS SQL, HSQLDB");
+        qualifications.add("Languages: Java, Scala, Python/Jython/PL-Python, JavaScript, Groovy");
+        qualifications.add("XML/XSD/XSLT, SQL, C/C++, Unix shell scripts");
+        qualifications.add("Java Frameworks: Java 8 (Time API, Streams), Guava, Java Executor, MyBatis, Spring (MVC, Security, Data, Clouds, Boot), JPA (Hibernate, EclipseLink), Guice, GWT(SmartGWT, ExtGWT/GXT), Vaadin, Jasperreports, Apache Commons, Eclipse SWT, JUnit, Selenium (htmlelements).");
+        qualifications.add("Python: Django.");
+        qualifications.add("JavaScript: jQuery, ExtJS, Bootstrap.js, underscore.js");
+        qualifications.add("Scala: SBT, Play2, Specs2, Anorm, Spray, Akka");
+        qualifications.add("Технологии: Servlet, JSP/JSTL, JAX-WS, REST, EJB, RMI, JMS, JavaMail, JAXB, StAX, SAX, DOM, XSLT, MDB, JMX, JDBC, JPA, JNDI, JAAS, SOAP, AJAX, Commet, HTML5, ESB, CMIS, BPMN2, LDAP, OAuth1, OAuth2, JWT.");
+        qualifications.add("Инструменты: Maven + plugin development, Gradle, настройка Ngnix");
+        qualifications.add("администрирование Hudson/Jenkins, Ant + custom task, SoapUI, JPublisher, Flyway, Nagios, iReport, OpenCmis, Bonita, pgBouncer");
+        qualifications.add("Отличное знание и опыт применения концепций ООП, SOA, шаблонов проектирования, архитектурных шаблонов, UML, функционального программирования");
+        qualifications.add("Родной русский, английский \"upper intermediate\"");
+        ListSection qualification = new ListSection(qualifications);
+        r.addSection(SectionType.QUALIFICATIONS, qualification);
 
-        List<Company> companyList = new ArrayList<>();
-        companyList.add(createCompany("Java Online Projects", "http://javaops.ru/", "Автор проекта", "Создание, организация и проведение Java онлайн проектов и стажировок.",
+        List<Company> companies = new ArrayList<>();
+        companies.add(createCompany("Java Online Projects", "http://javaops.ru/", "Автор проекта", "Создание, организация и проведение Java онлайн проектов и стажировок.",
                 2014, 10, LocalDate.now().getYear(), LocalDate.now().getMonthValue()));
-        companyList.add(createCompany("Wrike", "https://www.wrike.com/", "Старший разработчик (backend)", "Проектирование и разработка онлайн платформы управления проектами Wrike (Java 8 API, Maven, Spring, MyBatis, Guava, Vaadin, PostgreSQL, Redis). Двухфакторная аутентификация, авторизация по OAuth1, OAuth2, JWT SSO.",
+        companies.add(createCompany("Wrike", "https://www.wrike.com/", "Старший разработчик (backend)", "Проектирование и разработка онлайн платформы управления проектами Wrike (Java 8 API, Maven, Spring, MyBatis, Guava, Vaadin, PostgreSQL, Redis). Двухфакторная аутентификация, авторизация по OAuth1, OAuth2, JWT SSO.",
                 2013, 10, 2016, 1));
-        r.addSection(SectionType.EXPERIENCE, new CompanySection(companyList));
+        r.addSection(SectionType.EXPERIENCE, new CompanySection(companies));
 
-        List<Company> eduList = new ArrayList<>();
-        eduList.add(createCompany("Coursera", "https://www.coursera.org/course/progfun", "Functional Programming Principles in Scala' by Martin Odersky", "",
+        List<Company> educations = new ArrayList<>();
+        educations.add(createCompany("Coursera", "https://www.coursera.org/course/progfun", "Functional Programming Principles in Scala' by Martin Odersky", "",
                 2013, 3, 2013, 5));
-        r.addSection(SectionType.EDUCATION, new CompanySection(eduList));
+        r.addSection(SectionType.EDUCATION, new CompanySection(educations));
 
-        eduList.add(new Company("ИТМО", "http://www.ifmo.ru/", new ArrayList<Company.CompanyPeriod>(){{
+        educations.add(new Company("ИТМО", "http://www.ifmo.ru/", new ArrayList<Company.CompanyPeriod>(){{
             add(new Company.CompanyPeriod("Преподаватель", "", DateUtil.of(1996, 9), DateUtil.of(1997, 6)));
             add(new Company.CompanyPeriod("Аспирантура (программист С, С++)", "", DateUtil.of(1993, 9), DateUtil.of(1996, 7)));
             add(new Company.CompanyPeriod("Инженер (программист Fortran, C)", "", DateUtil.of(1987, 9), DateUtil.of(1993, 7)));
         }}));
-        eduList.add(createCompany("Luxoft", "http://www.luxoft-training.ru/training/catalog/course.html?ID=22366", "Курс 'Объектно-ориентированный анализ ИС. Концептуальное моделирование на UML.'", "",
+        educations.add(createCompany("Luxoft", "http://www.luxoft-training.ru/training/catalog/course.html?ID=22366", "Курс 'Объектно-ориентированный анализ ИС. Концептуальное моделирование на UML.'", "",
                 2011, 3, 2011, 4));
 
 
@@ -95,11 +92,6 @@ public class ResumeTestData {
         }});
     }
 
-    /*
-    Реализуйте в ResumeTestData метод, который будет принимать uuid и fullName, создавать резюме, заполнять его данными
-    (все секции резюме должны быть заполнены) и возвращать для тестирования в AbstractStorageTest.
-    Используйте его для всех резюме, создаваемых в AbstractStorageTest
-     */
     public static Resume createTestResume(String uuid, String fullName) {
         Resume resume = new Resume(uuid, fullName);
 
