6c4aefdd4004c9e440f6ead8e623eac72639d312
Cyril <efimov-ka@narod.ru>
2026-03-23 14:45:55 +0300

    HW 12 Java 8 Streams fixes

--- src/com/urise/webapp/util/StreamApiUtil.java
+++ src/com/urise/webapp/util/StreamApiUtil.java
@@ -7,6 +7,19 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 class StreamApiUtil {
+    static void main() {
+        int[] numArray = {7, 1, 8, 0, -1, 3, 1, 91, -2, 5, 3, 45, 8, 2};
+        System.out.println("Result of minValue:  " + minValue(numArray));
+        // в main вместо цикла используй стрим, возвращаемый List<Integer>
+        /* Integer[] boxedArray = new Integer[numArray.length];
+        for (int i = 0; i < numArray.length; i++) {
+            boxedArray[i] = numArray[i];
+        }
+        List<Integer> nums = Arrays.asList(boxedArray);*/
+        List<Integer> nums = List.of(7, 1, 8, 0, -1, 3, 1, 91, -2, 5, 3, 45, 8, 2);
+        System.out.println("Result of oddOrEven: " + oddOrEven(nums));
+    }
+
     static int minValue(int[] nums) {
         return IntStream.of(nums)
                 .filter(x -> x < 10)
@@ -17,20 +30,9 @@ class StreamApiUtil {
     }
 
     static List<Integer> oddOrEven(List<Integer> nums) {
-        Map<Boolean, List<Integer>> evenMap = nums.stream()
+        Map<Boolean, List<Integer>> oddOrEvenMap = nums.stream()
                 .collect(Collectors.partitioningBy(n -> n % 2 == 0));
-        if (evenMap.get(false).size() % 2 == 0) return evenMap.get(true);
-        return evenMap.get(false);
-    }
-
-    static void main() {
-        int[] numArray = {7, 1, 8, 0, -1, 3, 1, 91, -2, 5, 3, 45, 8, 2};
-        System.out.println("Result of minValue:  " + minValue(numArray));
-        Integer[] boxedArray = new Integer[numArray.length];
-        for (int i = 0; i < numArray.length; i++) {
-            boxedArray[i] = numArray[i];
-        }
-        List<Integer> numList = Arrays.asList(boxedArray);
-        System.out.println("Result of oddOrEven: " + oddOrEven(numList));
+        if (oddOrEvenMap.get(false).size() % 2 == 0) return oddOrEvenMap.get(true);
+        return oddOrEvenMap.get(false);
     }
 }
