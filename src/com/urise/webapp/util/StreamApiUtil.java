170680ba517916bf3859c7363f263c429460f7e5
Cyril <efimov-ka@narod.ru>
2026-03-25 16:09:12 +0300

    HW 12 Java 8 Streams fix2

--- src/com/urise/webapp/util/StreamApiUtil.java
+++ src/com/urise/webapp/util/StreamApiUtil.java
@@ -8,22 +8,16 @@ import java.util.stream.IntStream;
 
 class StreamApiUtil {
     static void main() {
-        int[] numArray = {7, 1, 8, 0, -1, 3, 1, 91, -2, 5, 3, 45, 8, 2};
+        int[] numArray = {7, 1, 8, 0, -1, 3, 1, 91, -2, 5, 3, 45, 8, 2, 3};
         System.out.println("Result of minValue:  " + minValue(numArray));
-        // в main вместо цикла используй стрим, возвращаемый List<Integer>
-        /* Integer[] boxedArray = new Integer[numArray.length];
-        for (int i = 0; i < numArray.length; i++) {
-            boxedArray[i] = numArray[i];
-        }
-        List<Integer> nums = Arrays.asList(boxedArray);*/
-        List<Integer> nums = List.of(7, 1, 8, 0, -1, 3, 1, 91, -2, 5, 3, 45, 8, 2);
+        List<Integer> nums = Arrays.stream(numArray).boxed().toList();
         System.out.println("Result of oddOrEven: " + oddOrEven(nums));
     }
 
     static int minValue(int[] nums) {
         return IntStream.of(nums)
-                .filter(x -> x < 10)
                 .filter(x -> x > 0)
+                .filter(x -> x < 10)
                 .distinct()
                 .sorted()
                 .reduce(0, (x, y) -> x * 10 + y);
