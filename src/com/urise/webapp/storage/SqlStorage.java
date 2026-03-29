8e0296942f8df8d4747e97d69fbb27c1f91b873e
Cyril <efimov-ka@narod.ru>
2026-03-29 20:21:17 +0300

    HW 13 not finished

--- src/com/urise/webapp/storage/SqlStorage.java
+++ src/com/urise/webapp/storage/SqlStorage.java
@@ -14,7 +14,43 @@ public class SqlStorage implements Storage {
         connectionFactory = () -> DriverManager.getConnection(dbUrl, dbUser, dbPassword);
     }
 
-    @Override
+    // insert/update/delete
+    protected void execute(String sql, SqlExecutor<Void> executor) {
+        try (Connection conn = connectionFactory.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            executor.accept(ps);
+            ps.execute();
+        } catch (SQLException e) {
+            throw new StorageException(e);
+        }
+    }
+
+    // select
+    protected <T> T executeQuery(String sql, SqlQueryExecutor<T> executor) {
+        try (Connection conn = connectionFactory.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ResultSet rs = executor.execute(ps);
+            return executor.map(rs);
+        } catch (SQLException e) {
+            throw new StorageException(e);
+        }
+    }
+
+    @FunctionalInterface
+    protected interface SqlExecutor<T> {
+        void accept(PreparedStatement ps) throws SQLException;
+    }
+
+    @FunctionalInterface
+    protected interface SqlQueryExecutor<T> {
+        ResultSet execute(PreparedStatement ps) throws SQLException;
+
+        default T map(ResultSet rs) throws SQLException {
+            return null;
+        }
+    }
+
+    /*@Override
     public void save(Resume r) {
         try (Connection conn = connectionFactory.getConnection();
                 PreparedStatement ps = conn.prepareStatement("INSERT INTO resume VALUES (?,?)")) {
@@ -37,14 +73,46 @@ public class SqlStorage implements Storage {
         } catch (SQLException e) {
             throw new StorageException(e);
         }
+    }*/
+    // Упрощенные методы
+    @Override
+    public void save(Resume r) {
+        execute("INSERT INTO resume VALUES (?,?)", ps -> {
+            ps.setString(1, r.getUuid());
+            ps.setString(2, r.getFullName());
+        });
+    }
+
+    @Override
+    public Resume get(String uuid) {
+        return executeQuery("SELECT * FROM resume r WHERE r.uuid = ?", new SqlQueryExecutor<Resume>() {
+            @Override
+            public ResultSet execute(PreparedStatement ps) throws SQLException {
+                ps.setString(1, uuid);
+                return ps.executeQuery();
+            }
+
+            @Override
+            public Resume map(ResultSet rs) throws SQLException {
+                if (!rs.next()) throw new NotExistStorageException(uuid);
+                return new Resume(uuid, rs.getString("full_name"));
+            }
+        });
     }
 
     @Override
     public void update(Resume r) {
+        execute("UPDATE resume r\n  SET r.full_name =? where r.uuid=?", ps -> {
+            ps.setString(1, r.getUuid());
+            ps.setString(2, r.getFullName());
+        });
     }
 
     @Override
     public void delete(String uuid) {
+        execute("DELETE FROM resume r\n where r.uuid=?", ps -> {
+            ps.setString(1, uuid);
+        });
     }
 
     @Override
---
+++ test/com/urise/webapp/storage/SqlStorageTest.java
@@ -0,0 +1,14 @@
+package com.urise.webapp.storage;
+
+import java.util.Properties;
+
+public class SqlStorageTest {
+    /*public SqlStorageTest() {
+        Properties prop = new Properties();
+        super(new SqlStorage(prop.getProperty("db.url"),prop.getProperty("db.user"),prop.getProperty("db.password")));
+
+
+
+    }*/
+
+}
