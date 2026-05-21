3ddc6a763086cd08e2985c43dd556663cf4e8f8c
Cyril <efimov-ka@narod.ru>
2026-05-22 00:08:35 +0300

    HW 14 test result fixes

--- src/com/urise/webapp/storage/SqlStorage.java
+++ src/com/urise/webapp/storage/SqlStorage.java
@@ -5,6 +5,7 @@ import com.urise.webapp.model.ContactType;
 import com.urise.webapp.model.Resume;
 import com.urise.webapp.sql.SqlHelper;
 import com.urise.webapp.sql.SqlQueryExecutor;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -45,10 +46,17 @@ public class SqlStorage implements Storage {
                 ps.setString(1, uuid);
                 return ps.executeQuery();
             }
+
             @Override
             public Resume map(ResultSet rs) throws SQLException {
                 if (!rs.next()) throw new NotExistStorageException(uuid);
-                return new Resume(uuid, rs.getString("full_name"));
+                Resume r = new Resume(uuid, rs.getString("full_name"));
+                do {
+                    String value = rs.getString("value");
+                    ContactType type = ContactType.valueOf(rs.getString("type"));
+                    r.addContact(type, value);
+                } while (rs.next());
+                return r;
             }
         });
     }
@@ -74,11 +82,8 @@ public class SqlStorage implements Storage {
 
     @Override
     public void delete(String uuid) {
-        int sqlResult = sqlHelper.execute("DELETE FROM resume where uuid = ?;\n" +
-                        "delete from contact where resume_uuid = ?", ps -> {
-                    ps.setString(1, uuid);
-                    ps.setString(2, uuid);
-                }
+        int sqlResult = sqlHelper.execute("DELETE FROM resume where uuid = ?;\n",
+                ps -> ps.setString(1, uuid)
         );
         if (sqlResult == 0) {
             throw new NotExistStorageException(null);
@@ -103,7 +108,16 @@ public class SqlStorage implements Storage {
 
     @Override
     public List<Resume> getAllSorted() {
-        return sqlHelper.executeQuery("SELECT * FROM resume \n order by full_name asc",
+        return sqlHelper.executeQuery("" +
+                        "select r.uuid, r.full_name, p.value as PHONE, e.value as EMAIL," +
+                        "       l.value as LINKEDIN, w.value as WEBSITE, g.value as GITHUB \n" +
+                        "  from resume r \n" +
+                        "  join public.contact p on r.uuid = p.resume_uuid and p.type = 'PHONE' \n" +
+                        "  join public.contact e on r.uuid = e.resume_uuid and e.type = 'EMAIL' \n" +
+                        "  join public.contact l on r.uuid = l.resume_uuid and l.type = 'LINKEDIN' \n" +
+                        "  join public.contact w on r.uuid = w.resume_uuid and w.type = 'WEBSITE' \n" +
+                        "  join public.contact g on r.uuid = g.resume_uuid and g.type = 'GITHUB'" +
+                        "order by r.full_name asc",
                 new SqlQueryExecutor<>() {
                     @Override
                     public ResultSet execute(PreparedStatement ps) throws SQLException {
@@ -112,10 +126,14 @@ public class SqlStorage implements Storage {
 
                     @Override
                     public List<Resume> map(ResultSet rs) throws SQLException {
-                        List<com.urise.webapp.model.Resume> result = new ArrayList<>();
+                        List<Resume> result = new ArrayList<>();
                         while (rs.next()) {
-                            result.add(new com.urise.webapp.model.Resume(rs.getString("uuid").trim(),
-                                    rs.getString("full_name")));
+                            Resume r = new Resume(rs.getString("uuid").trim(),
+                                    rs.getString("full_name"));
+                            for (ContactType ct : ContactType.values()) {
+                                r.addContact(ct, rs.getString(ct.toString()));
+                            }
+                            result.add(r);
                         }
                         return result;
                     }
